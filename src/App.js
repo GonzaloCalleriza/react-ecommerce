6cfba85fcc40689ed9ea9888b63e619c042bb6ac
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-02-05 19:06:14 +0100

    Incorporated addCart functionality

--- src/App.js
+++ src/App.js
@@ -5,20 +5,35 @@ import { NavBar, Products} from '/components';
 const App = () => {
   const [products, setProducts] = useState([]);
 
+  const [cart, setCart] = useState({});
+
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
 
     setProducts(data);
   }
 
+  const fetchCart = async () => {
+    const cart = await commerce.cart.retrieve();
+
+    setCart(cart);
+  }
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  }
+
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
 
   return (
     <div>
         <NavBar />
-        <Products products= {products}/>
+        <Products products= {products} onAddToCart={handleAddToCart} />
     </div>
   )
 };
